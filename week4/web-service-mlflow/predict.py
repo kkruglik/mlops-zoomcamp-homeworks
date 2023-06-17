389338c1c4cf5899f7df7a2b169223c54a1d37bc
Kirill Kruglikov <kirillkruglikov@outlook.com>
2023-06-17 15:01:05 +0000

    update week4

---
+++ week4/web-service-mlflow/predict.py
@@ -0,0 +1,16 @@
+import pickle
+
+with open('models/lin_reg.bin', 'rb') as f:
+    dv, model = pickle.load(f)
+
+def prepare_features(ride):
+    features = {}
+    features['PU_DO'] = '%s_%s' % (ride['PULocationID'], ride['DOLocationID'])
+    features['trip_distance'] = ride['trip_distance']
+    return features
+
+
+def predict(features):
+    X = dv.transform(features)
+    preds = model.predict(X)
+    return float(preds[0])
